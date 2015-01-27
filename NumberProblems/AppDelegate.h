5c7dc2e0277df770a105d6fea5bb22ab8757dfb8
Julien Guanzon <julienguanzon@gmail.com>
2015-01-26 18:56:36 -0700

    Initial Commit

---
+++ NumberProblems/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NumberProblems
+//
+//  Created by Julien Guanzon on 1/26/15.
+//  Copyright (c) 2015 Julien Guanzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NumberProblems/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NumberProblems
+//
+//  Created by Julien Guanzon on 1/26/15.
+//  Copyright (c) 2015 Julien Guanzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
